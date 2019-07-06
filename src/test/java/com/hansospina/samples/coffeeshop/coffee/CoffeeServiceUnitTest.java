935977fc91ee32c343d579e7021aee07a5a937b1
Hans Ospina <hansospina@gmail.com>
2019-07-06 11:44:41 -0500

    updated

--- src/test/java/com/hansospina/samples/coffeeshop/coffee/CoffeeServiceUnitTest.java
+++ src/test/java/com/hansospina/samples/coffeeshop/coffee/CoffeeServiceUnitTest.java
@@ -23,7 +23,7 @@ public class CoffeeServiceUnitTest {
 
     @Test
     public void testList() {
-        assertEquals(4, service.list().size());
+        assertEquals(5, service.list().size());
     }
 
     @Test
